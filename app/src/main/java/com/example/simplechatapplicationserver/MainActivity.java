6e3a85d5ba7b4975e045e92b05333e3275132a9a
Ethan Tai <etai@uchicago.edu>
2019-09-08 14:44:29 -0700

    initial commit

---
+++ app/src/main/java/com/example/simplechatapplicationserver/MainActivity.java
@@ -0,0 +1,83 @@
+package com.example.simplechatapplicationserver;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import java.io.*;
+import java.net.*;
+
+public class MainActivity extends AppCompatActivity {
+    TextView display;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        display = findViewById(R.id.textView);
+    }
+
+    public void connectServer(View view){
+        new connect().execute("");
+    }
+
+    public void changeText(String text){
+        display.setText(text);
+    }
+
+
+    private class connect extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... params) {
+
+            changeText("Waiting for connection");
+            try {
+                changeText("Try");
+                ServerSocket server = new ServerSocket(8008);
+                Socket socket = server.accept();
+                changeText("Connected.");
+                BufferedReader in = null;
+                in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                String msg = "";
+                while (!msg.equals("done")) {
+                    try {
+                        msg = in.readLine();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    if (msg == null) {
+                        break; // disconnected
+                    } else {
+                        changeText(msg);
+                        //break;
+                    }
+                }
+                changeText("Closing Socket.");
+                socket.close();
+                in.close();
+
+            }catch(IOException e){
+                changeText(e.getMessage());
+
+            }catch(Exception e){
+                changeText(e.getMessage());
+
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+        }
+
+        @Override
+        protected void onPreExecute() {
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+        }
+    }
+}
+
